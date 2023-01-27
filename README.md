c7a0911e2f9e123c838e36e3e433d5809a9b42ce
Jaardy <43437534+Jaardy@users.noreply.github.com>
2023-01-27 22:24:45 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# music-blitz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-pm4xfz)
---
+++ index.js
@@ -0,0 +1,150 @@
+// Import stylesheets
+import './style.css';
+import * as Tone from 'tone';
+
+// get buttons
+// const playButton = document.getElementById('play-button');
+// const stopButton = document.getElementById('stop-button');
+const sineOscillatorElement = document.getElementById('sine-oscillator');
+const triangleOscillatorElement = document.getElementById(
+  'triangle-oscillator'
+);
+const sawtoothOscillatorElement = document.getElementById(
+  'sawtooth-oscillator'
+);
+const squareOscillatorElement = document.getElementById('square-oscillator');
+const runExampleSynth = document.getElementById('run-example-synth');
+
+const pianoToggle = document.getElementById('piano-toggle')
+
+//create a synth and connect it to the main output (your speakers)
+// const synth = new Tone.Synth().toDestination();
+
+// =======================
+// EXAMPLE 1
+
+// const notesForSequence1 = ["C3", "G3", "G3", ["F3", "E3"], ["F3", "E3"], "D3", null]
+// const sequence = new Tone.Sequence((time, note,) => {
+//   synth.triggerAttackRelease(note, "2n", time)
+// }, notesForSequence1, "2n")
+// sequence.loop = false
+
+// const pattern = new Tone.Pattern(
+//   (time, { note, duration, delay, velocity }) => {
+//     // the order of the notes passed in depends on the pattern
+//     synth.triggerAttackRelease(note, duration, time + delay, velocity);
+//   },
+//   arrayOfNotesWithoutDelays,
+//   'up'
+// );
+
+// playButton.addEventListener('click', async () => {
+//   console.log('clicked!')
+//   await Tone.start()
+//   sequence.start()
+//   Tone.Transport.start();
+// });
+
+// stopButton.addEventListener('click', () => {
+//   Tone.Transport.stop();
+
+// });
+
+const playOscillator = async (e) => {
+  await Tone.start();
+
+  const type = e.target.innerHTML.toLowerCase();
+  const oscillator = new Tone.Oscillator(440, type).toDestination();
+  Tone.Transport.start();
+  oscillator.volume.value = '-20';
+  oscillator.start().stop('+0.7');
+  Tone.stop();
+};
+
+sineOscillatorElement.addEventListener('click', playOscillator);
+triangleOscillatorElement.addEventListener('click', playOscillator);
+sawtoothOscillatorElement.addEventListener('click', playOscillator);
+squareOscillatorElement.addEventListener('click', playOscillator);
+
+const playExampleSynth = async () => {
+  await Tone.Transport.start();
+  const synth = new Tone.Synth().toDestination();
+  synth.triggerAttackRelease('C4', '4n');
+};
+
+runExampleSynth.addEventListener('click', playExampleSynth);
+
+let synthForPiano = new Tone.PolySynth()
+pianoToggle.addEventListener('click', async (e) => {
+  if (e.target.checked) return synthForPiano.toDestination()
+  synthForPiano.releaseAll()
+  synthForPiano.disconnect()
+  
+})
+
+const getElementByNote = (note) => (note && document.querySelector(`[note="${note}"]`))
+const keys = {
+  A: { element: getElementByNote('C'), note: 'C', octaveOffset: 0 },
+  W: { element: getElementByNote('C#'), note: 'C#', octaveOffset: 0 },
+  S: { element: getElementByNote('D'), note: 'D', octaveOffset: 0 },
+  E: { element: getElementByNote('D#'), note: 'D#', octaveOffset: 0 },
+  D: { element: getElementByNote('E'), note: 'E', octaveOffset: 0 },
+  F: { element: getElementByNote('F'), note: 'F', octaveOffset: 0 },
+  T: { element: getElementByNote('F#'), note: 'F#', octaveOffset: 0 },
+  G: { element: getElementByNote('G'), note: 'G', octaveOffset: 0 },
+  Y: { element: getElementByNote('G#'), note: 'G#', octaveOffset: 0 },
+  H: { element: getElementByNote('A'), note: 'A', octaveOffset: 0 },
+  U: { element: getElementByNote('A#'), note: 'A#', octaveOffset: 0 },
+  J: { element: getElementByNote('B'), note: 'B', octaveOffset: 0 },
+  K: { element: getElementByNote('C2'), note: 'C', octaveOffset: 1 },
+  O: { element: getElementByNote('C#2'), note: 'C#', octaveOffset: 1 },
+  L: { element: getElementByNote('D2'), note: 'D', octaveOffset: 1 },
+  P: { element: getElementByNote('D#2'), note: 'D#', octaveOffset: 1 },
+  semicolon: { element: getElementByNote('E2'), note: 'E', octaveOffset: 1 },
+};
+
+const pressedNotes = new Map();
+let clickedKey = "";
+
+async function playPianoNote(e) {
+  console.log(e.key)
+  if (e.repeat || !pianoToggle.checked) return
+  const eventKey = e.key.toUpperCase()
+  const key = eventKey === ";" ? "semicolon" : eventKey
+  console.log(key)
+  if (!key || !keys[key]) {
+    return;
+  }
+
+  keys[key].element.classList.add("pressed");
+  
+  let note =keys[key].note
+  let octave = keys[key].octaveOffset + 3
+  
+  
+  synthForPiano.triggerAttack(note+octave)
+}
+
+async function stopPianoNote(e) {
+  const eventKey = e.key.toUpperCase()
+  const key = eventKey === ";" ? "semicolon" : eventKey
+  if (!key || !keys[key]) {
+    return;
+  }
+  keys[key].element.classList.remove("pressed");
+  let note =keys[key].note
+  let octave = keys[key].octaveOffset + 3
+  synthForPiano.triggerRelease(note+octave)
+}
+
+function lostWindowFocus () {
+  
+    console.log('lost focus')
+    if (synthForPiano) {
+      synthForPiano.releaseAll()
+    }
+}
+
+document.addEventListener("keydown", playPianoNote);
+document.addEventListener("keyup", stopPianoNote);
+window.addEventListener('blur', lostWindowFocus)
